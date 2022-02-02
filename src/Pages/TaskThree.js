2760d1cb910e5a85590ee514c1c501472592371e
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-02-02 20:39:56 +0600

    undone multiple connectionDB, mognoDB feild filtering, nodemailer email varification

--- src/Pages/TaskThree.js
+++ src/Pages/TaskThree.js
@@ -9,3 +9,14 @@ const TaskThree = () => {
 };
 
 export default TaskThree;
+
+
+
+// Task 3 : (Moderate)
+// You have to create two databases (student and teacher) in your
+// database server. You have to store basic information about students
+// & teachers in respective databases. You have to fetch the student
+// name from the student database and teacher name from the
+// teacher database and then print names in the console.
+// Hint: Create multi connection database.
+// Note: API http://localhost:3000/api/upcloud/users/
--- src/Pages/TaskTwo.js
+++ src/Pages/TaskTwo.js
@@ -1,11 +1,66 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+
 
 const TaskTwo = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    
+    const onSubmit = data => {
+        // console.log(data);
+
+        fetch('http://localhost:4040/storeUserInfo',{
+            method:'POST',
+            headers:{'Content-Type':'application/json'},
+            body: JSON.stringify(data)
+
+        }).then(data => console.log(data))
+
+    };
+
+
     return (
         <div>
-            
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <h1>Store User Information</h1>
+                <label htmlFor="name">Name</label> <br />
+                <input type='text'  placeholder='Name' {...register("name", { required: true })} /> <br />
+                {errors.name && <span>This field is required</span>} <br />
+                
+                <label htmlFor="email">Email</label> <br />
+                <input type='email'  placeholder='Email' {...register("email", { required: true })} /> <br />
+                {errors.email && <span>This field is required</span>} <br />
+
+                <label htmlFor="address">Address</label> <br />
+                <input type='text'  placeholder='address' {...register("address", { required: true })} /> <br />
+                {errors.address && <span>This field is required</span>} <br />
+
+                <label htmlFor="income">Income</label> <br />
+                <input type='number'  placeholder='Income' {...register("income", { required: true })} /> <br />
+                {errors.income && <span>This field is required</span>} <br />
+
+                <label htmlFor="maritalStatus">Marital Status</label> <br />
+                <input type='text'  placeholder='Marital Status' {...register("maritalStatus", { required: true })} /> <br />
+                {errors.maritalStatus && <span>This field is required</span>} <br />
+                
+                <label htmlFor="registrationNumber">Registration Number</label> <br />
+                <input type='number'  placeholder='Registration Number' {...register("registrationNumber", { required: true })} /> <br />
+                {errors.registrationNumber && <span>This field is required</span>} <br />
+
+                <label htmlFor="date">Date</label> <br />
+                <input type='date' placeholder='Date' {...register("date", { required: true })} /> <br />
+                {errors.date && <span>This field is required</span>} <br />
+                
+                <input type="submit" />
+            </form>
         </div>
     );
 };
 
 export default TaskTwo;
+
+
+
+// Create a database and store some information about the user like name, age, email, address, income,Marital Status, 
+// registration number and registration date etc. Now create an API which will show the only user name, age and email.
+// Note: API http://localhost:3000/api/upcloud/users/
